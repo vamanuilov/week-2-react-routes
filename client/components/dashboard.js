069c202103f8dd26ebea8386ef0b363dd4901e67
Vyacheslav Manuilov <vamanuilov@gmail.com>
2020-05-08 02:11:02 +0700

    Edit view

--- client/components/dashboard.js
+++ client/components/dashboard.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Dashboard = () => {
   return (
     <div>
-      <div className="flex items-center justify-center h-screen">
+      <div className="flex items-center justify-center">
         <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
           <div id="title">Dashboard</div>
           <Link to="/dashboard/profile/6d764b5b-a466-4370-ab6e-bf175b3828c6">Go To Profile</Link>
--- client/components/main.js
+++ client/components/main.js
@@ -6,7 +6,7 @@ const Main = () => {
   return (
     <div>
       <Head title="Hello" />
-      <div className="flex items-center justify-center h-screen">
+      <div className="flex items-center justify-center">
         <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
           <div id="title">Main</div>
           <Link to="/dashboard/profile/6d764b5b-a466-4370-ab6e-bf175b3828c6">Go To Profile</Link>
