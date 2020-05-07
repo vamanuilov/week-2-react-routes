cf7abba973d38de5e59090757c61d0faf2d133a1
Vyacheslav Manuilov <vamanuilov@gmail.com>
2020-05-08 01:05:46 +0700

    Removed last / from urls

--- client/components/home.js
+++ client/components/home.js
@@ -11,7 +11,7 @@ const Home = () => {
       <Header />
       <div className="flex items-center justify-center h-screen">
         <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
-          <Route exact path="/dashboard/" component={() => <Dashboard />} />
+          <Route exact path="/dashboard" component={() => <Dashboard />} />
           <Route exact path="/dashboard/main" component={() => <Main />} />
           <Route exact path="/dashboard/profile/:username" component={() => <Profile />} />
         </div>
--- client/components/profile.js
+++ client/components/profile.js
@@ -9,8 +9,8 @@ const Profile = () => {
       <Head title="Hello" />
       <div className="flex items-center justify-center h-screen">
         <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
-          <Link to="/dashboard/main/">Go To Main</Link>
-          <Link to="/dashboard/">Go To Root</Link>
+          <Link to="/dashboard/main">Go To Main</Link>
+          <Link to="/dashboard">Go To Root</Link>
           <div id="title">Main</div>
           <div id="username">{username}</div>
         </div>
