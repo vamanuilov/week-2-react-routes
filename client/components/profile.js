fdb5a7fe1d7bbd9e95669e481aa6b7921a21f6e8
Vyacheslav Manuilov <vamanuilov@gmail.com>
2020-05-08 00:47:37 +0700

    Add components

---
+++ client/components/profile.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Link, useParams } from 'react-router-dom'
+import Head from './head'
+
+const Profile = () => {
+  const { username } = useParams()
+  return (
+    <div>
+      <Head title="Hello" />
+      <div className="flex items-center justify-center h-screen">
+        <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
+          <Link to="/dashboard/main/">Go To Main</Link>
+          <Link to="/dashboard/">Go To Root</Link>
+          <div id="title">Main</div>
+          <div id="username">{username}</div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+Profile.propTypes = {}
+
+export default Profile
