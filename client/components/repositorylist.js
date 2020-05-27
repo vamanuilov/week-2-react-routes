ec52dc7e95a2c89bc5667d71167b5d62146a03fd
Vyacheslav <vamanuilov@gmail.com>
2020-05-27 22:23:29 +0700

    minor refactor

--- client/components/repositorylist.js
+++ client/components/repositorylist.js
@@ -6,10 +6,12 @@ const RepositoryList = (props) => {
     <div className="flex flex-col items-center p-3">
       {props.repositories.map((repo) => {
         return (
-          <Link key={repo.id} to={`/${repo.full_name}`}>
-            <div className="flex rounded-md items-center bg-teal-500 hover:bg-teal-700 text-white m-1 p-4">
-              {repo.name}
-            </div>
+          <Link
+            className="flex rounded-md items-center bg-teal-500 hover:bg-teal-700 text-white m-1 p-4"
+            key={repo.id}
+            to={`/${repo.full_name}`}
+           >
+             {repo.name}
           </Link>
         )
       })}
